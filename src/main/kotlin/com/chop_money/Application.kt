4cbd84d679fc11b136f74d05d3326937d36d33b0
gymcode <kyleabs20@gmail.com>
2022-10-18 22:44:09 +0000

    initial push for chop-money

---
+++ src/main/kotlin/com/chop_money/Application.kt
@@ -0,0 +1,13 @@
+package com.chop_money
+
+import com.chop_money.core.plugins.configureRouting
+import com.chop_money.core.plugins.configureSerialization
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSerialization()
+        configureRouting()
+    }.start(wait = true)
+}
